234453de97b63b87b0ced5172de7123e0a97c915
pengpengyang <lyana.q669@gmail.com>
2023-09-05 14:07:26 +0300

    variables and do sum and print

--- src/homework.java
+++ src/homework.java
@@ -2,8 +2,11 @@
 public class homework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		System.out.println("hello");
+		int x = 5;
+		int y = 6;
+		System.out.println(x + y);
+		
+		}
 
 }
